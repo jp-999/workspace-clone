ee6f6f22a22638cd2c89c988564c5cd81702e427
jp-999 <jayanthpeddi999@gmail.com>
2025-03-31 17:30:08 +0530

    analytics

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react"
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './components/navigation/Navbar';
